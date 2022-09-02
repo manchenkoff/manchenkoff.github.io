170ce89413998c46a7ab88be428173b221d942cf
manchenkov <artyom@manchenkoff.me>
2022-09-02 02:58:25 +0200

    linter fixes

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="w-28 h-28">
-    <img src="~/assets/img/logo.png" alt="Artem Manchenkov" class="w-full h-full object-cover">
+    <img
+      src="~/assets/img/logo.png"
+      alt="Artem Manchenkov"
+      class="w-full h-full object-cover"
+    />
   </div>
 </template>
